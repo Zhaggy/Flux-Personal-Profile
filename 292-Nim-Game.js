892a6d7365eb380771beff8ed95bd4df2e252b0c
Zhaggym <zhaggm@gmail.com>
2016-12-03 12:07:06 +0100

    292-Nim-Game

---
+++ 292-Nim-Game.js
@@ -0,0 +1,8 @@
+var canWinNim = function(n) {
+    return n%4 ? true : false;
+};
+
+console.log(canWinNim(10));
+
+// RUNTIME: 128 ms
+// COUNT: 3
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## Daily Algorithm Practice
+## Algorithm Practice
