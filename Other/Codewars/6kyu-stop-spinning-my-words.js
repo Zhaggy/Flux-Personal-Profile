27b4c44c8a02869a7eaa2b5a6b714c25415daf74
Zhaggym <zhaggm@gmail.com>
2016-12-25 08:37:09 +0100

    6kyu-stop-spinning-my-words solved

--- Other/Codewars/6kyu-stop-spinning-my-words.js
+++ Other/Codewars/6kyu-stop-spinning-my-words.js
@@ -2,3 +2,17 @@
 // the same string, but with all five or more letter words reversed (Just like
 // the name of this Kata). Strings passed in will consist of only letters and
 // spaces. Spaces will be included only when more than one word is present.
+
+const spinWords = str => {
+  return str.split(' ').map((c, i, a) => {
+    if (c.length >= 5) {
+      return c.split('').reverse().join();
+    }
+    return c;
+  }).join(' ');
+}
+
+console.log(spinWords('Stop Spinning my words'));
+
+
+// let spinWords=s=>s.split(' ').map(c=>c.length>4?c.split('').reverse().join(''):c).join(' ');
