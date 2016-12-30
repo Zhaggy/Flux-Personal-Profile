130fbfad33c819723662c2fbee52996a0bcef0e6
Zhaggym <zhaggm@gmail.com>
2016-12-30 11:00:49 +0100

    07-Rotate-Matrix init

---
+++ CtCI/01-Arrays-And-Strings/07-Rotate-Matrix.js
@@ -0,0 +1,18 @@
+// Given an image represented by an NxN matrix, where each pixel in the image
+// is 4 bytes, write a method to rotate the image by 90 degrees. (an you do
+// this in place?
+
+
+
+/*
+[ [ a, b, c, d ],
+  [ e, f, g, h ],
+  [ i, j, k, l ],
+  [ m, n, o, p ] ]
+
+  [ [ m, i, e, a ],
+    [ n, j, f, b ],
+    [ o, k, g, c ],
+    [ p, l, h, d ] ]
+
+*/
