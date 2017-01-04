8b2228328c1d5921bb0eda2ae82d42544870c079
Zhaggym <zhaggm@gmail.com>
2017-01-04 14:41:33 +0100

    5kyu-persistent-bugger init

---
+++ Other/Codewars/5kyu-persistent-bugger.js
@@ -0,0 +1,12 @@
+// Write a function, persistence, that takes in a positive parameter num and
+// returns its multiplicative persistence, which is the number of times you
+// must multiply the digits in num until you reach a single digit.
+
+
+const persistence = n => {
+  // let n = num;
+  while (n > 9) {
+
+  }
+  return n;
+}
