2c9d0e649c939d1514ca986ad0c147a416d18e2e
Zhaggym <zhaggm@gmail.com>
2017-01-06 16:15:35 +0100

    6kyu-equal-sides-of-an-array solution

--- Other/Codewars/6kyu-equal-sides-of-an-array.js
+++ Other/Codewars/6kyu-equal-sides-of-an-array.js
@@ -4,5 +4,19 @@
 // that would make this happen, return -1
 
 const findEvenIndex = arr => {
-
+  if (!arr.length) return -1;
+  let  right = 0, left = arr.reduce((sum, n) => sum + n) - arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    right += arr[i - 1];
+    left -= arr[i];
+    if (right === left) return i;
+  }
+  return -1;
 }
+
+console.log(findEvenIndex([1,2,3,4,3,2,1]));
+// 3
+console.log(findEvenIndex([1,100,50,-51,1,1]));
+// 1
+console.log(findEvenIndex([1,2,3,4,5,6]));
+// -1
