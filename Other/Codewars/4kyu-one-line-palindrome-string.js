95752035c076cdedaa42f17870e4efc6da471243
Zhaggym <zhaggm@gmail.com>
2017-01-08 15:29:44 +0100

    4kyu-one-line-palindrome-string init

---
+++ Other/Codewars/4kyu-one-line-palindrome-string.js
@@ -0,0 +1,7 @@
+// Your task is to generate a palindrome string, using the specified length n,
+// the specified characters c (all characters in c must be used at least once),
+// and the code length should less than 62 characters.
+
+palindrome=(n,c)=> {
+  
+};
