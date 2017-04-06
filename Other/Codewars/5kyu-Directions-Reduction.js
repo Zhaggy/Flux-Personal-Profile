06050dc21d4c607358264de09d5966271e3da602
Zhaggym <zhaggm@gmail.com>
2017-04-06 13:21:09 +0200

    Directions Reduction Solved

--- Other/Codewars/5kyu-Directions-Reduction.js
+++ Other/Codewars/5kyu-Directions-Reduction.js
@@ -1,7 +1,26 @@
-// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+// Write a function dirReduc which will take an array of strings and returns an
+// array of strings with the needless directions removed (W<->E or S<->N side by side).
 
 const dirReduc = arr => {
-  return arr.reduce((acc, c, i, a) => {
-
-  });
+  let going = true;
+  while (going) {
+    let changes = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (shouldDelete(arr[i], arr[i + 1])) {
+        arr.splice(i, 2);
+        changes++;
+      }
+    }
+    if (changes === 0) going = false;
+  }
+  return arr;
 }
+
+const shouldDelete = (a, b) => {
+  if ((a === "NORTH" && b === "SOUTH") || (a === "SOUTH" && b === "NORTH") || (a === "EAST" && b === "WEST") || (a === "WEST" && b === "EAST")) return true;
+  else return false;
+}
+
+
+console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]));
+// SHOULD LOG [ "WEST" ]
