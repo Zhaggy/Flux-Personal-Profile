cce12bf777c33dfd1c7da74682d7b43e50b6b26f
Zhaggym <zhaggm@gmail.com>
2017-04-06 12:47:48 +0200

    Directions Reduction CW init

---
+++ Other/Codewars/5kyu-Directions-Reduction.js
@@ -0,0 +1,7 @@
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+const dirReduc = arr => {
+  return arr.reduce((acc, c, i, a) => {
+
+  });
+}
